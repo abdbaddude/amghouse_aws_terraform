177c03fd5e2ed0e33cc5f87b0a5d60a329ed16b5
Abdurrahman Adebiyi <adebiyi.abdurrahman@amghousecorp.com>
2020-08-22 02:00:41 +0100

    Add a .gitignore file

---
+++ amghouse_terrafrom_tasks.sh
@@ -0,0 +1,23 @@
+ terraform init
+ terraform plan  -out amghouse_terrafrom_plan
+ terraform apply "amghouse_terrafrom_plan"
+ terraform show
+
+
+git init
+git add main.tf
+git status
+git commit -m "Initial commit"
+git status
+cat > .gitignore <<EOF
+.terraform
+*.tfstate
+*.tfstate.backup
+EOF
+git add .gitignore
+git add amghouse_terrafrom_*
+git status
+git commit -m "Add a .gitignore file"
+git status
+git remote -v add origin git@github.com:abdbaddude/amghouse_aws_terraform.git
+git push -v origin master
